4e6c67fded87c8e74eb09a6f68938938c1e25613
dlawler2 <dlawler2@angelo.edu>
2015-08-03 14:05:48 -0500

    Second Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.assets.precompile += ['static_pages.css']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
